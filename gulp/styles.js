7c9364a25213d6594e89083fa4634fbcbcf8027c
feizheng <1290657123@qq.com>
2016-10-20 13:00:42 +0800

    refactor

--- gulp/styles.js
+++ gulp/styles.js
@@ -12,7 +12,15 @@
   //styles
   gulp.task('styles',function() {
     return gulp.src('src/*.scss')
+      .pipe(gulp.dest('dist'))
+      .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+      .pipe(gulp.dest('dist'))
+      .pipe($.sass({outputStyle: 'compressed'}).on('error', $.sass.logError))
+      .pipe($.rename({
+        extname:'.min.css'
+      }))
       .pipe(gulp.dest('dist'));
   });
+
 
 }());
